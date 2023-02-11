413b537470e766439f56801be2b80b12848d0634
Israel Hamdan <IsraelSantos_20K@outlook.com>
2023-02-11 15:59:57 -0300

    aula 4

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -4,16 +4,17 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './formulario.css'
 
-const Formulario = ({aoCadastrar, times}) => {
+const Formulario = ({aoCadastrar, times, cadastrarTime}) => {
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
 
     const aoSubmeter = (evento) => {
         evento.preventDefault()
-        console.log('form enviado', nome, cargo, imagem, time )
         aoCadastrar({
             nome,
             cargo,
@@ -24,6 +25,7 @@ const Formulario = ({aoCadastrar, times}) => {
 
     return (
         <section className="formulario-container">
+            {/*criando um novo colaborador*/}
             <form className="formulario" onSubmit={aoSubmeter}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
                 <CampoTexto
@@ -31,24 +33,47 @@ const Formulario = ({aoCadastrar, times}) => {
                     label='Nome'
                     placeholder='Digite seu nome '
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
+                    aoAlterado={valor => setNome(valor)}
+                />
                 <CampoTexto
                     obrigatorio={true}
                     label='Cargo' 
                     placeholder='Digite seu cargo '
                     valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}/>
+                    aoAlterado={valor => setCargo(valor)}
+                />
                 <CampoTexto 
                     label='Imagem' 
                     placeholder='Informe o endereço da imagem '
-                    aoAlterado={valor => setImagem(valor)}/>
+                    aoAlterado={valor => setImagem(valor)}
+                />
                 <ListaSuspensa 
                     obrigatorio={true}
                     label='Times'
                     items={times} 
                     valor={time}
-                    aoAlterado={valor => setTime(valor)}/>
+                    aoAlterado={valor => setTime(valor)}
+                />
                 <Botao texto='Criar card' />
+            </form>
+            {/*criando um novo time*/}
+            <form className="formulario" onSubmit={(evento) => {evento.preventDefault(); cadastrarTime({nome: nomeTime, cor:corTime})} }>
+                <h2>Preencha os dados para criar um novo time.</h2>
+                <CampoTexto
+                    obrigatorio
+                    label='Nome'
+                    placeholder='Digite o nome do time '
+                    valor={nome}
+                    aoAlterado={valor => setNomeTime(valor)}
+                />
+                <CampoTexto
+                    obrigatorio
+                    label='Cargo' 
+                    placeholder='Digite a cor do time '
+                    valor={cargo}
+                    aoAlterado={valor => setCorTime(valor)}
+                />
+                <Botao texto='Criar time' />
             </form>
         </section>
     )
--- src/componentes/Time/index.jsx
+++ src/componentes/Time/index.jsx
@@ -3,18 +3,18 @@ import './time.css'
 import hexToRgba from 'hex-to-rgba';
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
-    console.log(time.cor)
     return (
         colaboradores.length > 0 && <section className='time' style={{ 
                 backgroundImage: 'url(/imagens/fundo.png)', 
                 backgroundColor: hexToRgba(time.cor, '0.6')
-                
-            }}
-            >
+            }}>
+            
             <input 
-                onChange={evento => mudarCor(evento.target.value, time.nome)} 
-                type="color" value={time.cor} 
+                onChange={evento => mudarCor(evento.target.value, time.id)} 
+                type="color" 
+                value={time.cor} 
                 className='input-color' 
+
             />
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
